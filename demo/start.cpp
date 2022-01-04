87515b489fd59424f5d724bd469b46badfe3005f
rktrlng <rik.teerling@gmail.com>
2022-01-04 19:23:19 +0100

    start

---
+++ demo/start.cpp
@@ -0,0 +1,93 @@
+/**
+ * @file particles.cpp
+ *
+ * @brief particles implementation
+ *
+ * Copyright 2021-2022 @rktrlng
+ * https://github.com/rktrlng/canvas
+ */
+
+#include <ctime>
+#include <deque>
+
+#include <canvas/application.h>
+
+class MyApp : public rt::Application
+{
+public:
+	MyApp(uint16_t width, uint16_t height, uint8_t bitdepth, uint8_t factor) : rt::Application(width, height, bitdepth, factor)
+	{
+		std::srand(std::time(nullptr));
+	}
+
+	// MyApp(rt::PixelBuffer& pixelbuffer, uint8_t factor) : rt::Application(pixelbuffer, factor)
+	// {
+	// 
+	// }
+
+	virtual ~MyApp()
+	{
+		
+	}
+
+	void update(float deltatime) override
+	{
+		handleInput();
+
+		static float frametime = 0.0f;
+		float maxtime = 0.01667f - deltatime;
+		frametime += deltatime;
+		if (frametime >= maxtime) {
+			updatePixels();
+			frametime = 0.0f;
+		}
+	}
+
+private:
+	void updatePixels()
+	{
+		auto& pixelbuffer = layers[0]->pixelbuffer;
+		size_t rows = pixelbuffer.header().height;
+		size_t cols = pixelbuffer.header().width;
+
+		for (size_t y = 0; y < rows; y++) {
+			for (size_t x = 0; x < cols; x++) {
+				pb::RGBAColor color = pb::RGBAColor(rand()%255, rand()%255, rand()%255, 255);
+				pixelbuffer.setPixel(x, y, color);
+			}
+		}
+		pixelbuffer.blur();
+		layers[0]->lock();
+	}
+
+	void handleInput()
+	{
+		if (input.getKeyDown(rt::KeyCode::Space)) {
+			std::cout << "spacebar pressed down." << std::endl;
+			layers[0]->pixelbuffer.printInfo();
+		}
+
+		if (input.getMouseDown(0)) {
+			std::cout << "click " << (int) input.getMouseX() << "," << (int) input.getMouseY() << std::endl;
+		}
+
+		int scrolly = input.getScrollY();
+		if (scrolly != 0) {
+			std::cout << "scroll: " << scrolly << std::endl;
+		}
+	}
+
+};
+
+
+int main( void )
+{
+	MyApp application(320, 180, 24, 4);
+
+	while (!application.quit())
+	{
+		application.run();
+	}
+
+	return 0;
+}
